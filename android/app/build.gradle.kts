bf7a809628ca1fe183129d0681612dd38119a13a
Vishal Raj <vishalraj27439@gnail.com>
2026-05-08 12:47:35 +0530

    chngs and added admin

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.shiwalikeducation.apppro"
         minSdk = flutter.minSdkVersion
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
     }
 
     compileOptions {
